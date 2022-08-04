26db8eea02ac3daebeae815ba24b8b32eced8748
Alma Powell <mac@macs-MacBook-Pro.local>
2022-08-04 14:17:30 -0500

    added landing page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import LandingPage from "./components/LandingPage";
 
 const App = () => {
-  return <div>Landing Page</div>;
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  );
 };
 
 export default App;
