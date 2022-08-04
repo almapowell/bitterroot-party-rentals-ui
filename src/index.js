37e58cf093d751dbfbe8ff4c188b847e7120aba8
Alma Powell <mac@macs-MacBook-Pro.local>
2022-08-04 14:01:49 -0500

    add header

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -13,6 +14,7 @@ root.render(
     <Provider store={store}>
       <BrowserRouter>
         <div className="App">
+          <Header />
           <Routes>
             <Route path="/" element={<App />} />
           </Routes>
