13642a90a6a6024317e14e52df77cc3a25f4159c
Alma Powell <mac@macs-MacBook-Pro.local>
2022-08-04 13:13:41 -0500

    removed categories

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import Header from "./components/Header";
-import Categories from "./components/Categories";
-import SelectedCategory from "./components/Categories/SelectedCategory";
 import LandingPage from "./components/LandingPage";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { categories } from "./shared/utils";
 import Admin from "./components/Admin";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -22,26 +19,8 @@ root.render(
 
           <Routes>
             <Route path="/" element={<App />} />
-            <Route path="/categories" element={<Categories />} />
             <Route path="/landing-page" element={<LandingPage />} />
             <Route path="/jordy" element={<Admin />} />
-
-            <Route
-              path={`/${categories[0].link}`}
-              element={<SelectedCategory items={categories[0]} />}
-            />
-            <Route
-              path={`/${categories[1].link}`}
-              element={<SelectedCategory items={categories[1]} />}
-            />
-            <Route
-              path={`/${categories[2].link}`}
-              element={<SelectedCategory items={categories[2]} />}
-            />
-            <Route
-              path={`/${categories[3].link}`}
-              element={<SelectedCategory items={categories[3]} />}
-            />
           </Routes>
         </div>
       </BrowserRouter>
