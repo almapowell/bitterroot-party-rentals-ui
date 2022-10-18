3ca950a1ac4106ed1ce6d9fcd14a0f1f9cd1a232
Alma Powell <mac@macs-MacBook-Pro.local>
2022-10-18 16:50:30 -0500

    added new images to mason format

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -6,6 +6,12 @@ import ImageListItem from "@mui/material/ImageListItem";
 import { Image } from "antd";
 import "./styles.css";
 import { useNavigate } from "react-router";
+import img1 from "../../shared/images/landing-page.jpg";
+import img2 from "../../shared/images/imageTwo.JPG";
+import img3 from "../../shared/images/imageThree.JPG";
+import img4 from "../../shared/images/imageFour.JPG";
+import img5 from "../../shared/images/imageFive.JPG";
+import img6 from "../../shared/images/imageSix.JPG";
 
 const LandingPage = () => {
   const navigate = useNavigate();
@@ -70,51 +76,51 @@ export default LandingPage;
 
 const itemData = [
   {
-    img: "https://images.unsplash.com/photo-1549388604-817d15aa0110",
+    img: img1,
     title: "Bed",
   },
   {
-    img: "https://images.unsplash.com/photo-1525097487452-6278ff080c31",
+    img: img2,
     title: "Books",
   },
   {
-    img: "https://images.unsplash.com/photo-1523413651479-597eb2da0ad6",
+    img: img3,
     title: "Sink",
   },
   {
-    img: "https://images.unsplash.com/photo-1563298723-dcfebaa392e3",
+    img: img4,
     title: "Kitchen",
   },
   {
-    img: "https://images.unsplash.com/photo-1588436706487-9d55d73a39e3",
+    img: img5,
     title: "Blinds",
   },
   {
-    img: "https://images.unsplash.com/photo-1574180045827-681f8a1a9622",
+    img: img6,
     title: "Chairs",
   },
-  {
-    img: "https://images.unsplash.com/photo-1530731141654-5993c3016c77",
-    title: "Laptop",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1481277542470-605612bd2d61",
-    title: "Doors",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1517487881594-2787fef5ebf7",
-    title: "Coffee",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee",
-    title: "Storage",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62",
-    title: "Candle",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1519710164239-da123dc03ef4",
-    title: "Coffee table",
-  },
+  // {
+  //   img: img7,
+  //   title: "Laptop",
+  // },
+  // {
+  //   img: "https://images.unsplash.com/photo-1481277542470-605612bd2d61",
+  //   title: "Doors",
+  // },
+  // {
+  //   img: "https://images.unsplash.com/photo-1517487881594-2787fef5ebf7",
+  //   title: "Coffee",
+  // },
+  // {
+  //   img: "https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee",
+  //   title: "Storage",
+  // },
+  // {
+  //   img: "https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62",
+  //   title: "Candle",
+  // },
+  // {
+  //   img: "https://images.unsplash.com/photo-1519710164239-da123dc03ef4",
+  //   title: "Coffee table",
+  // },
 ];
