0ea2b83d8904cee45b0e9039f279d08ac50ef14c
Alma Powell <alma111000@gmail.com>
2022-11-24 15:32:11 -0600

    added new landing page layout

--- src/components/LandingPage/WhyChooseUs/index.js
+++ src/components/LandingPage/WhyChooseUs/index.js
@@ -3,21 +3,42 @@ import "./styles.css";
 import { phone, store, worker } from "./utils";
 
 const WhyChooseUs = () => {
+
+  const alternatePhotos = (index) => {
+
+    return index % 2 === 0 || window.innerWidth;
+  }
+
   return (
     <div>
       <div className="why">
         <span className="main-title">Why Work With Us?</span>
         <div className="section-container">
-          {reasons.map((reason) => (
-            <section>
-              <div className="description">
-                <div>
-                  <span className="icon-container">{reason.icon}</span>
+          {reasons.map((reason, index) => (
+            <section
+            className="section"
+              style={{display: 'grid', gridTemplateColumns: '1fr 1fr'}}
+            >
+              {index % 2 !== 0 && window.innerWidth > 1000 && <div className="image">
+                <img src={reason.img} />
+              </div>}
+
+              <div className="textContainer">
+                <div className="description">
+                  <div>
+                    <span className="icon-container">{reason.icon}</span>
+                  </div>
+                  <span className="title">{reason.title}</span>
                 </div>
-                <span className="title">{reason.title}</span>
+                <p className="subText">{reason.description}</p>
               </div>
-              <p className="subText">{reason.description}</p>
-              <img style={{ height: 200 }} src={reason.img} />
+
+              {index % 2 === 0 && <div className="image">
+                <img src={reason.img} />
+              </div>}
+               {index % 2 !== 0 && window.innerWidth < 1000 && <div className="image">
+                <img src={reason.img} />
+              </div>}
             </section>
           ))}
         </div>
@@ -28,50 +49,42 @@ const WhyChooseUs = () => {
 
 export default WhyChooseUs;
 
+const itemsImages = [
+   "https://drive.google.com/uc?export=view&id=1S84VOzz3trPFshR7XFoS7QUO1TJ-yC0E",
+   "https://drive.google.com/uc?export=view&id=1cGTQBrLr4Aneh8CW4SE6g9WGoMZuvM5V",
+   "https://drive.google.com/uc?export=view&id=1TmMjUi9Q56iDwvcvf1nhMaAqkrRvFi49",
+   "https://drive.google.com/uc?export=view&id=1XJwH1NnZ1645F_il5YYYFKntB2OLlNvv",
+   "https://drive.google.com/uc?export=view&id=1aDKA5QoRBTHmOu5hgswcagDQ0iePKRNY",
+   "https://drive.google.com/uc?export=view&id=1viglfl2V1XKthyRFaFQhPahNExwUd-RX",
+   "https://drive.google.com/uc?export=view&id=1ck-euKuuFTgAL_RGY3x_zujBkKO3KPKN",
+   "https://drive.google.com/uc?export=view&id=1HisUWEJ2MQTl_YJCYQnBMwv5DD2qLicJ",
+   "https://drive.google.com/uc?export=view&id=17pf47SRWozmjLdN2Jl0KnMDwWXLHGqJo",
+];
+
+console.log(itemsImages[0])
+
 const reasons = [
   {
     icon: phone,
     title: "Great Customer Service",
     description:
       "We offer personal contact information which allows a very direct and quick point of contact.",
-    img: "https://drive.google.com/uc?export=view&id=1S84VOzz3trPFshR7XFoS7QUO1TJ-yC0E",
+    img: itemsImages[0],
   },
   {
     icon: store,
     title: "Local Small Business",
     description:
       "Started in 2019, we are a family opporated business trying serving the Bitterroot/Missoula area.",
-    img: "https://drive.google.com/uc?export=view&id=1cGTQBrLr4Aneh8CW4SE6g9WGoMZuvM5V",
+    img: itemsImages[1],
   },
   {
     icon: worker,
     title: "We Do The Work For You",
     description:
       "We offer to set up and take down equipment for you so that your event can be as hassle free as possible.",
-    img: "https://drive.google.com/uc?export=view&id=1aDKA5QoRBTHmOu5hgswcagDQ0iePKRNY",
+    img: itemsImages[2],
   },
 ];
 
-const itemData = [
-  {
-    img: "https://drive.google.com/uc?export=view&id=1S84VOzz3trPFshR7XFoS7QUO1TJ-yC0E",
-  },
-  {
-    img: "https://drive.google.com/uc?export=view&id=1cGTQBrLr4Aneh8CW4SE6g9WGoMZuvM5V",
-  },
-  {
-    img: "https://drive.google.com/uc?export=view&id=1aDKA5QoRBTHmOu5hgswcagDQ0iePKRNY",
-  },
-  {
-    img: "https://drive.google.com/uc?export=view&id=1viglfl2V1XKthyRFaFQhPahNExwUd-RX",
-  },
-  {
-    img: "https://drive.google.com/uc?export=view&id=1ck-euKuuFTgAL_RGY3x_zujBkKO3KPKN",
-  },
-  {
-    img: "https://drive.google.com/uc?export=view&id=1HisUWEJ2MQTl_YJCYQnBMwv5DD2qLicJ",
-  },
-  {
-    img: "https://drive.google.com/uc?export=view&id=17pf47SRWozmjLdN2Jl0KnMDwWXLHGqJo",
-  },
-];
+
