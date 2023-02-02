887bd1a4354f41100e661e012a7206e767d5e777
Alma Powell <alma111000@gmail.com>
2023-02-02 09:23:50 -0600

    removed moment from email controller

--- server/controllers/emailController.js
+++ server/controllers/emailController.js
@@ -1,5 +1,5 @@
 const aws = require("aws-sdk");
-const moment = require("moment");
+// const moment = require("moment");
 const ses = new aws.SES({ region: "us-east-1" });
 
 exports.contactForm = async (req, res) => {
@@ -97,7 +97,7 @@ Customer Information: \n
   Deliver: ${delivery ? "Yes" : "No"}\n
   Referal: ${getReferal()}\n
   Notes: ${notes}\n
-  Date: ${moment(date).format("MMMM Do YYYY")}\n
+  Date: ${date}\n
 \n
 
 From: \n${emailFrom}`,
